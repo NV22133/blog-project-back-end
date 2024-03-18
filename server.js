12cad89d06b0e576b47a062143784249a14bd691
NV22133 <155047853+NV22133@users.noreply.github.com>
2024-03-18 08:33:57 +0000

    Back-end

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-const express = require("express");
-
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res)  => {res.send('Server is running...')})
+
+const PORT = proccess.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server is running on port ${PORT}'))
+
